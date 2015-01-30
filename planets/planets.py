b76fa597f3e01050d35fb25170518ea5519a7bfe
useanalias <use.an.alias@gmail.com>
2015-01-29 22:02:47 -0800

    Add docstrings.

--- planets/planets.py
+++ planets/planets.py
@@ -5,30 +5,45 @@ import pygame.gfxdraw
 from pygame.locals import *
 from scipy.optimize import fsolve
 
+
 def get_screen_size():
+    """Returns the current screen size."""
     info = pygame.display.Info()
     return (info.current_w, info.current_h)
 
+
 def get_screen_middle():
+    """Returns the middle of the current screen."""
     size = get_screen_size()
-    return (size[0]/2.0, size[1]/2.0)
- 
+    return (size[0] / 2.0, size[1] / 2.0)
+
+
 class Sun:
+
+    """An unmoving celestial sphere."""
+
     def __init__(self, radius, image_name, position=None):
+        """Generates a new Sun."""
         self.radius = radius
         self.color = color
-        self.position = position 
+        self.position = position
         image = pygame.image.load(os.path.join('images', image_name + '.png'))
-        self.image = pygame.transform.scale(image, [radius*2, radius*2])
+        self.image = pygame.transform.scale(image, [radius * 2, radius * 2])
 
     def update(self, t):
+        """Does nothing; simply for compatability with Planets."""
         pass
 
-class Planet: 
+
+class Planet:
+
+    """A moving celestial sphere following Kepler's equations."""
+
     def __init__(self, radius, image_name, semi_major_axis, period, eccentricity, sun):
+        """Generates a new Planet."""
         self.radius = radius
         image = pygame.image.load(os.path.join('images', image_name + '.png'))
-        self.image = pygame.transform.scale(image, [radius*2, radius*2])
+        self.image = pygame.transform.scale(image, [radius * 2, radius * 2])
         self.semi_major_axis = semi_major_axis
         self.period = period
         self.eccentricity = eccentricity
@@ -37,32 +52,39 @@ class Planet:
         if self.sun.position is not None:
             self.center = self.sun.position
 
-
     def update(self, t):
-       # Compute the mean motion
-       n = 2.0 * math.pi / self.period
-       M = n * t
-       # Compute the eccentric anomaly
-       epsilon = self.eccentricity
-       E = fsolve(lambda E: E - epsilon * math.sin(E) - M, 0.0)
-       # Compute the true anomaly
-       theta = fsolve(lambda theta: (1.0-epsilon) * math.tan(theta/2.0)**2 - (1.0 + epsilon) * math.tan(E/2.0) ** 2, 2*math.pi/self.period*t)
-       # Compute the heliocentric distance
-       a = self.semi_major_axis
-       r = a * (1.0 - epsilon * math.cos(E)) 
-       x = r * math.cos(theta)
-       y = r * math.sin(theta)
-       if self.center is None:
-           self.center = get_screen_middle()
-       self.position = (int(x) + self.center[0], int(y) + self.center[1])
+        """Updates the position of the Planet."""
+        # Compute the mean motion
+        n = 2.0 * math.pi / self.period
+        M = n * t
+        # Compute the eccentric anomaly
+        epsilon = self.eccentricity
+        E = fsolve(lambda E: E - epsilon * math.sin(E) - M, 0.0)
+        # Compute the true anomaly
+        theta = fsolve(lambda theta: (1.0 - epsilon) * math.tan(theta / 2.0) **
+                       2 - (1.0 + epsilon) * math.tan(E / 2.0) ** 2, 2 * math.pi / self.period * t)
+        # Compute the heliocentric distance
+        a = self.semi_major_axis
+        r = a * (1.0 - epsilon * math.cos(E))
+        x = r * math.cos(theta)
+        y = r * math.sin(theta)
+        if self.center is None:
+            self.center = get_screen_middle()
+        self.position = (int(x) + self.center[0], int(y) + self.center[1])
+
 
 class System:
+
+    """A system of celestial spheres composed of sun(s) and planet(s)."""
+
     def __init__(self, bodies, color):
-        self.bodies = bodies 
+        """Generates a new System."""
+        self.bodies = bodies
         self.color = color
         self.time = 0
 
     def run(self):
+        """Runs the system."""
         pygame.init()
         self.screen_size = get_screen_size()
         screen = pygame.display.set_mode(self.screen_size, pygame.FULLSCREEN)
@@ -73,17 +95,15 @@ class System:
             for body in self.bodies:
                 body.update(self.time)
                 if body.position is None:
-                    body.position = get_screen_middle() 
+                    body.position = get_screen_middle()
                 x = body.position[0] - body.radius
                 y = body.position[1] - body.radius
-                screen.blit(body.image, (x, y)) 
+                screen.blit(body.image, (x, y))
 
-            self.time += 1 
+            self.time += 1
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
-                    pygame.quit() 
+                    pygame.quit()
                     quit = True
-
-
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python
+from setuptools import setup
+
+setup(
+    name = 'planets',
+    version = '1.0',
+    description = 'A pygame based planetary model generator.',
+    packages = ['planets'],
+    include_package_data = True,
+)
