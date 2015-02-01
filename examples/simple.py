63ce321a09f2d94f7dc5c8c5f88f4162e8e20ef5
useanalias <use.an.alias@gmail.com>
2015-01-31 19:06:58 -0800

    Fix examples

--- examples/simple.py
+++ examples/simple.py
@@ -2,8 +2,11 @@ from planets.planets import *
 
 # Entirely not to scale.
 # For an exercise, fit to actual parameters.
+# Warning: if you set the eccentricity too high, the circle approxmation
+# for theta given to fsolve will deviate from the actual path enough 
+# to cause the planet to flash between both sides of the orbit.
 
 sun = Sun(100, 'sun')
-earth = Planet(10, 'earth', 1000, 1000, 0.5, sun)
+earth = Planet(10, 'earth', 1000, 1000, 0.2, sun)
 system = System([sun, earth], (0, 0, 0)) 
 system.run()
